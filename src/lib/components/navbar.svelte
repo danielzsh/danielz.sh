2dd1619ebbcb4dd8d24b7ad4c05df47d92cec6ec
devo1ution <danielfangzhu@gmail.com>
2024-01-23 23:14:49 -0800

    semi-working

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,6 +1,14 @@
 <script>
 	import logo from '$lib/images/pfp.png'
 	import { page } from '$app/stores'
+	const links = $page.url.pathname
+		.split('/')
+		.slice(1)
+		.reduce((res, link) => {
+			res.push(res.length ? [link, res.at(-1)[0] + '/' + link] : [link || '~', `/${link}`])
+			return res
+		}, [])
+	console.log(links)
 </script>
 
 <div class="w-full h-auto text-7xl flex align-middle">
@@ -8,5 +16,9 @@
 		<img src={logo} alt="logo" class="rounded-full h-[4.5rem] w-auto" />
 	</a>
 	<p class="text-gray-600 font-medium">/</p>
-	<p class="text-white font-extralight">{$page.url.pathname?.slice(1) || '~'}</p>
+	<div class="font-extralight text-white">
+		{#each links as [link, href]}
+			<a {href} class="heading">{link}</a>
+		{/each}
+	</div>
 </div>
