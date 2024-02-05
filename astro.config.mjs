ce6b75f52cf12e87e988620ed6ee3f40f4ebcba1
devo1ution <danielfangzhu@gmail.com>
2024-02-04 21:25:37 -0800

    get rid of ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import Inspect from 'vite-plugin-inspect';
-import svelte from '@astrojs/svelte';
-import vercel from '@astrojs/vercel/serverless';
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import Inspect from 'vite-plugin-inspect'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwind(), svelte(), react()],
+  integrations: [tailwind(), react()],
   vite: {
     plugins: [Inspect()]
-  },
-  adapter: vercel({
-    imageService: true
-  })
-});
+  }
+})
